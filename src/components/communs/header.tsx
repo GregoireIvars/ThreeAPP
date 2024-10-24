89efb6ffdacc24a8f05398852593c64da1accfd7
gregoire.ivars <gregoire.ivars@inetum.com>
2024-10-24 15:42:37 +0200

    feat: three js implémentation

--- src/components/communs/header.tsx
+++ src/components/communs/header.tsx
@@ -1,26 +1,35 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <header className="bg-blue-800 text-white py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center px-6">
-        <h1 className="text-2xl font-bold">Grégoire IVARS</h1>
-        <nav>
-          <ul className="flex space-x-6">
-            <li>
-              <Link to="/" className="hover:text-blue-300 transition-colors duration-200">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/services" className="hover:text-blue-300 transition-colors duration-200">Prestations</Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:text-blue-300 transition-colors duration-200">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+interface HeaderProps {
+    onLinkClick: (event: React.MouseEvent<HTMLAnchorElement>, path: string) => void; // Interface pour les props
 }
+
+const Header: React.FC<HeaderProps> = ({ onLinkClick }) => {
+    return (
+        <header className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-black text-white text-center p-2">
+            <h1 className="text-2xl font-bold">Grégoire IVARS</h1>
+            <nav>
+                <ul className="flex space-x-6">
+                    <li>
+                        <Link to="/" onClick={(event) => onLinkClick(event, '/')} className="hover:text-blue-300 transition-colors duration-200">
+                            Accueil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/services" onClick={(event) => onLinkClick(event, '/services')} className="hover:text-blue-300 transition-colors duration-200">
+                            Prestations
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" onClick={(event) => onLinkClick(event, '/contact')} className="hover:text-blue-300 transition-colors duration-200">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
